991dc549eaa5140f907ff02377426bcb1f0ed8ff
Kalan Prudhomme <cleaver9@gmail.com>
2021-04-28 14:40:52 -0700

    simple structure

---
+++ src/App/App.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import 'App.css';
+import React from 'react';
+import Header from 'Header';
+import Footer from 'Footer';
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        <Header/>
+
+        <main>
+
+        </main>
+
+        <Footer/>
+
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/App/Footer.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+import 'Footer.css';
+import React from 'react';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer className="Footer">
+        <h2>The wonderful world of Pokemon</h2>
+      </footer>
+    );
+  }
+
+}
+
+export default Footer;
